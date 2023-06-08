e84d12c91ddd641c83217e24ff0a44418357d76c
mono-sunami <sunami@monosus.co.jp>
2023-06-08 10:54:50 +0900

    csrf

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 const express = require('express');
-const api = require('./routes/api');
+const csrf = require('./routes/csrf');
 const app = express();
-const appAPI = express();
 const port = 3000;
-const portAPI = 3001;
 
-app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
-appAPI.use(
-  express.static('public_target', {
-    // ハンズオン①
-    // setHeaders: (res, path, stat) => {
-    //   res.set('X-Frame-Options', 'SAMEORIGIN');
-    // },
+app.use(
+  express.static('public', {
+    setHeaders: (res, path, stat) => {
+      res.header('X-Frame-Options', 'SAMEORIGIN');
+    },
   })
 );
 
-// appAPI.use('/api', api);
+app.use('/csrf', csrf);
+
+app.get('/', (req, res, next) => {
+  res.end('Top Page');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-});
-
-appAPI.listen(portAPI, () => {
-  console.log(`Server is running on http://localhost:${portAPI}`);
 });
